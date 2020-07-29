37fe1d7a74e84f72722558f1c1c3a9c37db2dcd3
Tim Beyer <tim.beyer@gmail.com>
2020-07-30 00:13:25 +0200

    fix: lower map a bit so all scans fit without being cut off

--- src/components/MapPlane.tsx
+++ src/components/MapPlane.tsx
@@ -12,7 +12,11 @@ export const MapPlane: React.FC = (props) => {
   const meshRef = useRef<THREE.Mesh>()
   const texture = useLoader(THREE.TextureLoader, baseMap)
   return (
-    <mesh ref={meshRef} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
+    <mesh
+      ref={meshRef}
+      rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+      position={[0, -100, 0]}
+    >
       <planeGeometry attach="geometry" args={[MAP_WIDTH, MAP_HEIGHT]} />
       <meshBasicMaterial
         attach="material"
